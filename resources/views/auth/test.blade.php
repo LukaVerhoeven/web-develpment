e7b7cdad545fb225061ee8c98eb037d4b1b2528a
LukaVerhoeven <lka.v.lv@gmail.com>
2016-11-02 19:43:50 +0100

    test

---
+++ resources/views/auth/test.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('title')
+  Login
+@stop
+
+@section('content')
+<div class="container">
+    <div class="row centered-form">
+        <div class="col-md-8 col-md-offset-2">
+        <div class="returnlink">
+            <a href="/overzicht"> Terug naar overzicht</a>
+        </div>
+            <div class="panel panel-default loginform">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">E-Mail</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>We kunnen je niet inloggen met wat je net hebt ingevoerd. Probeer nog een keer.</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Wachtwoord</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                   Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
